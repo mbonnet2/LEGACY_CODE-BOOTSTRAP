591c83b0b9973338a3dfefcc3f436f9e77019545
nicolas <nicolas.deschaseaux@epitech.eu>
2025-09-05 13:01:38 +0200

    FEAT: cobol tests

---
+++ tests/legacy_cobol_tests/constants.py
@@ -0,0 +1,6 @@
+import os
+
+
+EXECUTABLE = os.path.abspath("../modernize-legacy-cobol-app-main/accountsystem")#"/usr/bin/python3 ../python_app/main.py")
+PROMPT_MESSAGE = r"Enter your choice \(1-4\):"
+EXIT_MESSAGE = "Exiting the program. Goodbye!"
